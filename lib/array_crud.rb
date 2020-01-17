6794ea9cc96e83831c2dca72b4ca1ade2d7af7b7
Spencer Blum <spencerblum@gmail.com>
2020-01-17 20:31:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array2 = ["floor", "ceiling"]
+  array2 = ["floor", "ceiling", "fox", 1]
 end
 
 def add_element_to_end_of_array(array, element)
